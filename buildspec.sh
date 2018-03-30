252ff8bd5f6b68b37afe94084fce78ce5f73ca87
Alex Ough <alex.ough@sungardas.com>
2018-03-29 20:57:20 -0500

    added account permission lambda function

---
+++ buildspec.sh
@@ -0,0 +1,15 @@
+
+cd src; npm install; cd ..
+
+aws cloudformation package \
+   --template-file ./template.yaml \
+   --s3-bucket $S3_BUCKET_NAME \
+   --output-template-file samTemplate.yaml
+
+aws cloudformation deploy --template-file ./samTemplate.yaml \
+  --capabilities CAPABILITY_IAM \
+  --stack-name SungardAS-aws-services-alerts \
+  --parameter-overrides AlertKinesisDestinationName=$ALERT_KINESIS_DESTINATION_NAME \
+  AlertMessageDynamoDBTableName=$ALERT_MESSAGE_DYNAMODB_TABLE_NAME \
+  SlackWebHookUrl=$SLACK_WEBHOOK_URL SlackChannel=$SLACK_CHANNEL \
+  TeamsWebHookUrl=$TEAM_WEBHOOK_URL
